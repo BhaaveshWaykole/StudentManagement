827c2a5aa800c84dc43eb944ca3c2310ab053a55
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-04-02 00:09:46 +0530

    students -attendance schema change

--- api/models/Student.js
+++ api/models/Student.js
@@ -24,25 +24,28 @@ const studentSchema = new mongoose.Schema({
     photo: {
         type: String
     },
-    attendance: [
-        {
-            classroom_id: {
-                type: String,
-                required: true
-            },
-            total_present: {
-                type: Number,
-                required: true
-            },
-            total_classes: {
-                //count of how many classes happened. needed for total attendance .
-                type: Number,
-                required: true
+    attendance: {
+        type: Array,
+        default: [
+            {
+                classroom_id: {
+                    type: String,
+                    required: true
+                },
+                total_present: {
+                    type: Number,
+                    required: true
+                },
+                total_classes: {
+                    //count of how many classes happened. needed for total attendance .
+                    type: Number,
+                    required: true
+                }
             }
-        }
-    ]
+        ]
+    }
 },
 );
 
-const student = mongoose.model("Students", studentSchema)
+const student = mongoose.model("Student", studentSchema)
 export default student;
