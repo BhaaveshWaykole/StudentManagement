9c24b2328fb9e54c3efc7ed07162f272f276c1a4
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-04-01 20:08:10 +0530

    tailwind added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Zian
+        <h1 className="italic text-3xl text-orange-900">WELCOME</h1>
       </header>
     </div>
   );
