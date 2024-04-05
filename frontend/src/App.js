6569372322bf3998d4eaa5375203082aa7ff399d
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-04-05 11:26:25 +0530

    minor change -overlook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import Navbar from './components/navbar/Navbar.jsx'
 import Home from './pages/home/Home.jsx';
 
-// using map -> map the classes on hompage using the goToClassroom page when clicked redirect to main classroom page.
-
 function App() {
   return (
     <div className="App">
