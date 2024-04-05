0c13543f9f6574f2c9a64dd1ff2fb4831d67641f
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-04-05 23:09:09 +0530

    announcements multiple display

--- frontend/src/pages/classroom/Classroom.jsx
+++ frontend/src/pages/classroom/Classroom.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 import AnnouncementCard from '../../components/announcements/AnnouncementCard.jsx'
 
 export default function Clasroom() {
+  const goToAnnouncement = (announcementId) => {
+    console.log(announcementId);
+  };
+
+  let announcements = [
+    {
+      id: 1,
+      title: "Welcome to the Class!",
+      teachers: "Vipin T",
+      content: "Dear students, welcome to our class. I'm excited to start this journey with you all. Please make sure to review the syllabus and let me know if you have any questions.",
+      date: "April 5, 2024"
+    },
+    {
+      id: 2,
+      title: "Assignment Submission Reminder",
+      teachers: "Deepak D",
+      content: "Just a reminder that the deadline for the first assignment is approaching. Make sure to submit it before the end of the week. Good luck!",
+      date: "April 10, 2024"
+    },
+    {
+      id: 3,
+      title: "Office Hours Update",
+      teachers: "Parija B",
+      content: "I've updated my office hours for this semester. Please check the schedule on the course website and feel free to drop by if you have any questions or concerns.",
+      date: "April 7, 2024"
+    }
+  ];
+
   return (
-    <div className="Main-corousel bg-red-500 h-screen mx-10 mt-10 rounded-rnd-6p">
+    <div className="Main-corousel bg-red-500 mx-10 mt-10 rounded-rnd-6p">
 
       <div className="Main-Name bg-amber-600 h-60 p-3 rounded-rnd-6p">
         <h1 className="text-center font-poppins-500 text-3xl">
@@ -20,12 +48,17 @@ export default function Clasroom() {
         <input type="text" placeholder='Post Announcement here' className="font-poppins-200 rounded-rnd-6 bg-transparent border-b border-black w-full mt-12 focus:outline-none" />
       </div>
 
-      <div className="font-poppins-500 bg-white my-3 mx-5 p-3 rounded-rnd-6p">
+      <div className="font-poppins-500">
         <a href="">
-          <AnnouncementCard />
+          <div className="flex flex-row flex-wrap">
+            {announcements.map((announcementItem) => (
+              <div className="border-2 border-black w-full my-3 mx-5 p-3 rounded-rnd-6p bg-white" key={announcementItem.key} onClick={() => goToAnnouncement(announcementItem.key)}>
+                <AnnouncementCard announceInfo={announcementItem} />
+              </div>
+            ))}
+          </div>
         </a>
       </div>
-
     </div>
   )
 }
