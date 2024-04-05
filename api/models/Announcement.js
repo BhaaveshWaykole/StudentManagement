9cb7bf3782ba20bc2857a518a9328f3722d0ce75
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-04-06 00:05:11 +0530

    announcement model

---
+++ api/models/Announcement.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const announcementSchema = new mongoose.Schema({
+    aId: {
+        type: String
+    },
+    teacher: {
+        type: String,
+        required: true
+    },
+    content : {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    }
+},
+    { timestamps: true }
+)
+
+const announcement = mongoose.model("announcement", announcementSchema)
+export default announcement
