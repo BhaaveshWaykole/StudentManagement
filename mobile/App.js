c55c60d5ad4d6b8c8b906129c4340427bf6e1405
yashjugade <109807948+yashjugade@users.noreply.github.com>
2024-04-17 02:36:52 +0530

    Announcement &Attendance

--- mobile/App.js
+++ mobile/App.js
@@ -1,29 +1,20 @@
-// App.js
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-// import { AuthProvider } from './context/AuthContext.js';
-// import Navbar from './screens/Navbar.js';
-//import ClassNavbar from './screens/ClassNavbar.js';
-import Navbar from './screens/Navbar';
-import { View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import AttendanceCard from './screens/AttendanceCard';
 
-const Stack = createStackNavigator();
-
-const App = () => {
+export default function App() {
   return (
-    // <NavigationContainer>
-    //   <AuthProvider>
-    //     <Stack.Navigator>
-    //       <Stack.Screen name="Login" component={Login} />
-    //       {/* Add other screens here */}
-    //     </Stack.Navigator>
-    //   </AuthProvider>
-    // </NavigationContainer>
-    <View>
-      <Navbar/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <AttendanceCard />
+    </SafeAreaView>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+  },
+});
+
